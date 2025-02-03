288897ef1e61b000f1ee1f1181c3502f1e7828ae
tanishabamne <tanisha.bamne1980@gmail.com>
2025-02-03 09:18:38 -0800

    second

--- dictionary_function.py
+++ dictionary_function.py
@@ -2,13 +2,29 @@ d={'name':'John','age':25}
 # print(d)
 # print(type(d))
 
-print(max(d))
-print(min(d))
-print(len(d))
-print(id(d))
-print(type(d))
+# print(max(d))
+# print(min(d))
+# print(len(d))
+# print(id(d))
+# print(type(d))
 
-x=dict()
-print(x)
-print(type(x))
+# x=dict()
+# print(x)
+# print(type(x))
 
+#dictionary methods
+d1={'name':'tanisha','age':19,'quali':'b-tech'}
+# print(id.get('name'))
+
+# print(d1.keys())
+# print(d1.values())
+# print(d1.items())
+
+# d1.pop('name')
+# print(d1)
+
+# d1.popitem()
+# print(d1)
+# d1.setdefault('name','tanu')
+d1.update({'name':'tanu'})
+print(d1)
