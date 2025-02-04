3295ac48c6a2e4c8fd7d0dde709572d68c73c1d6
tanishabamne <tanisha.bamne1980@gmail.com>
2025-02-04 09:02:23 -0800

    set methods and functions

--- set.py
+++ set.py
@@ -1,5 +1,40 @@
-st ={10,20,'tanisha','jai','neha'}
-print(st)
-print(type(st))
+# st ={10,20,'tanisha','jai',10,20,'neha'}
+# print(st)
+# print(type(st))
 
-s2={10,20,'tanisha','jai','neha}
+# #  set functions
+st2={10,20,30,40,50,60}
+# print(max(st2))
+# print(min(st2))
+# print(sum(st2))
+# print(id(st2))
+# print(len(st2))
+# _________________________________________________________set methods_____________________________
+# x=st2.copy()
+# print(id(x),id(st2))
+
+# st2.add(80)
+# print(st2)
+
+    # st2.update('tanisha')
+    # print(st2)
+    # pop
+
+# st2.discard(80)
+# print(st2)
+
+# ________________________________mathematical method________________________________________
+a={2,4,6,7,8,0}
+b={1,3,5,7.8,9}
+
+# print(a.union(b))
+# print(a.intersection(b))
+# print(a.difference(b))
+# print(a.union(b))
+# print(a.symmetric_difference(b))
+
+a.intersection_update(b)
+print(a)
+
+
+
