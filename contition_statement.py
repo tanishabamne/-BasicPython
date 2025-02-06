ae6afd0ce9159bb7f643c602a23f63646e630bfe
tanishabamne <tanisha.bamne1980@gmail.com>
2025-02-06 09:28:27 -0800

    conditional _statement

---
+++ contition_statement.py
@@ -0,0 +1,24 @@
+# ____________________________if____________________________
+# n=int(input("enter any no"))
+# if (n%2==0):
+#     print(f'{n}even no')
+# print('hello')
+# ________________________if_else___________________________
+
+# n=int(input("enter any no"))
+# if (n%2==0):
+#     print(f'{n}even no')
+# else:
+#     print(f'{n}odd no')
+
+    # __________if_elseif____________
+
+n=int(input("enter your percentage:"))
+if (n>=60): 
+    print(f'{n} is 1st division')
+elif (n>=45 and n<60):
+    pprint(f'{n} is 2nd division')
+
+elif (n>=35 and n<45):
+    pprint(f'{n} is 3nd division')
+      
--- frozenset.py
+++ frozenset.py
@@ -1,6 +1,37 @@
 #kisi bhi collection ko freez krne k liye ise use krte hai 
-fset1= frozenset({'monkey','10,20','tanisha'})
-fset2= frozenset({'tanisha','10,20','monkey'})
-print(id(fset1),id(fset2))             #True       
+# fset1= frozenset({'monkey','10,20','tanisha'})
+# fset2= frozenset({'tanisha','10,20','monkey'})
+# print(id(fset1),id(fset2))             #True       
 
-#frozenset is immutable                             
+#frozenset is immutable  
+# collection of uniqe elemnt 
+# frees any collection (list,set,tuple,string)
+# not indexing support 
+# not slicing support 
+#unorderd
+
+#_______________ methods_________________________
+# s='tanisha'
+# fs=frozenset(s)
+# print(fs)
+# print(type(fs))
+
+# s={10,20,30,40,50}
+# l={2,4,8,30,4,20}
+# x=frozenset(s)
+# y=frozenset(l)
+# print(x.union(y))
+# print(y.intersection(x))
+# print(x.difference(y))
+
+# s1='tanisha'
+# s2='bamne'
+# x=frozenset(s1)
+# print(x)
+# y=frozenset(s2)
+# print(y)
+
+x={1,2,3,4,5,6}
+y={1,2,3,4,6,7,8,0}
+print(y.issuperset(x))
+print(x.issubset(y))
